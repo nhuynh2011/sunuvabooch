2777995556b250184776d5341c875fc213535448
Nick Huynh <nick.huynh@uconn.edu>
2022-07-15 11:07:22 -0400

    Product Carousel Stub

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { Component, OnInit, HostListener, Inject, ElementRef, ViewChild } from '@angular/core';
 import { trigger, state, transition, style, animate } from '@angular/animations';
 import { DOCUMENT } from '@angular/common';
+import KeenSlider, { KeenSliderInstance } from "keen-slider"
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: [
+    '../../../node_modules/keen-slider/keen-slider.min.css',
+    './home.component.css'
+  ],
   animations:[
     trigger('fade',
       [
@@ -20,6 +24,35 @@ export class HomeComponent implements OnInit {
   constructor(@Inject(DOCUMENT) document : any) { }
 
   ngOnInit() {
+  }
+  @ViewChild("sliderRef") sliderRef: ElementRef<HTMLElement>
+
+  currentSlide: number = 0
+  dotHelper: Array<Number> = []
+  slider: KeenSliderInstance = null
+
+
+  ngAfterViewInit() {
+    let slidesPerView = 3;
+
+    this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+      initial: this.currentSlide,
+      loop: true,
+      mode: "free-snap",
+      slides: {
+        perView: slidesPerView
+      },
+      slideChanged: (s) => {
+        this.currentSlide = s.track.details.rel
+      }
+    })
+    this.dotHelper = [
+      ...Array(this.slider.track.details.slides.length).keys(),
+    ]
+  }
+
+  ngOnDestroy() {
+    if (this.slider) this.slider.destroy()
   }
 
   @HostListener('window:scroll', ['$event'])
